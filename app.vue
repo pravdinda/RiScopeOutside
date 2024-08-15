d5261c3f96bce8e74dac557eb4986e065febc3ca
Roman Ismagilov <roman.j123@ya.ru>
2024-08-15 13:56:32 +0300

    add i18n

--- app.vue
+++ app.vue
@@ -4,3 +4,6 @@
     <NuxtWelcome />
   </div>
 </template>
+<script setup lang="ts">
+console.log(process.env);
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ["@nuxt/image"],
+  devtools: { enabled: process.env.NODE_ENV === 'development' },
+  modules: ["@nuxt/image", "@nuxtjs/i18n"],
   css: ['~/assets/scss/main.scss']
 })
