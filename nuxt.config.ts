43ad692700ed45267c507cab1d0a3d901fa46d07
Roman Ismagilov <roman.j123@ya.ru>
2024-08-15 15:13:54 +0300

    add NuxtLayout, useHead, fonts, global styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: process.env.NODE_ENV === 'development' },
   modules: ["@nuxt/image", "@nuxtjs/i18n"],
-  css: ['~/assets/scss/main.scss']
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/index.scss" as *;'
+        }
+      }
+    }
+  },
+  css: ['normalize.css/normalize.css', '~/assets/scss/main.scss']
 })
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>RISCOPE</h1>
+</template>
+<script setup lang="ts">
+useHead({
+  title: "RIScope",
+  meta: [
+    { name: "description", content: "Risk management automation Service" },
+  ],
+});
+</script>
